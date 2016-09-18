2b232b8476fac716f53387bf0e1fac2553b83b3f
Arthur Giuliano <arthur.giuliano@student.42.fr>
2016-09-18 18:21:10 +0200

    GO

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: agiulian <arthur.giuliano@student.42.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/13 21:05:20 by agiulian          #+#    #+#             */
-/*   Updated: 2016/09/17 21:43:01 by agiulian         ###   ########.fr       */
+/*   Updated: 2016/09/18 03:59:46 by agiulian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	ft_atoi(const char *str)
 
 	i = 0;
 	negative = 0;
+	result = 0;
 	while (str[i] <= 32 || str[i] == 127)
 		i++;
 	if (str[i] == '-')
@@ -27,7 +28,7 @@ int	ft_atoi(const char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result *= 10;
-		result = str[i] - '0';
+		result += str[i] - '0';
 		i++;
 	}
 	if (negative == 1)
